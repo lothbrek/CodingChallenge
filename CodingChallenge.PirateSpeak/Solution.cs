57a4c1d1c1c4993f81e94c7c52d9d0bcd1533166
Matt McCullar <1687383+lothbrek@users.noreply.github.com>
2023-11-09 15:42:20 -0600

    Update Solution.cs
    Update PirateSpeak Challenge with solution

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
 {
     public class Solution
-    {
-        public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            public string GetPossibleWords(string scrambled, string[] dictionary)
+            {
+                // sort the scrambled word letters
+                var sortedScrambled = String.Concat(scrambled.OrderBy(c => c));
+
+                // use LINQ to find all words in the dictionary that, when sorted, match the sorted scrambled word
+                var matches = dictionary.Where(word => String.Concat(word.OrderBy(c => c)) == sortedScrambled);
+
+                // return the joined matching words into a single string, separated by a comma and a space
+                return string.Join(", ", matches);
+            }
         }
-    }
 }

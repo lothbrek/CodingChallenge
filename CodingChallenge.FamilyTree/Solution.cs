18090351661a90f9de38106050277e24e385aec2
Matt McCullar <1687383+lothbrek@users.noreply.github.com>
2023-11-09 15:43:00 -0600

    Update Solution.cs
    Update FamilyTree Challenge with solution

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -6,7 +6,32 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            // check if the person argument is null
+            if (person == null)
+            {
+                return null;
+            }
+
+            // check if the current person is the one we're looking for
+            if (person.Name == descendantName)
+            {
+                return person.Birthday.ToString("MMMM");
+            }
+
+            // if not, recursively search through this person's descendants
+            foreach (Person descendant in person.Descendants)
+            {
+                string birthMonth = GetBirthMonth(descendant, descendantName);
+
+                // if a non-empty string is returned, we've found the person
+                if (!string.IsNullOrEmpty(birthMonth))
+                {
+                    return birthMonth;
+                }
+            }
+
+            // person was not found in the family tree
+            return null;
         }
     }
 }
